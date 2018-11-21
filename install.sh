32fa5a26cf30010d6fec622bf81596e615c0a117
Stefan Rua <stefu.rua@gmail.com>
2018-11-21 16:57:33 +0200

    add tint2 as default panel

--- install.sh
+++ install.sh
@@ -2,12 +2,12 @@
 
 ln -s $PWD/.Xresources $HOME/
 
-for i in bspwm sxhkd rofi dunst ranger
+for i in bspwm sxhkd rofi dunst ranger tint2
 do
 touch $HOME/.config/$i
 done
 
-for i in bspwm sxhkd rofi dunst ranger
+for i in bspwm sxhkd rofi dunst ranger tint2
 do
 ln -s $PWD/$i $HOME/.dotfiles/
 done
