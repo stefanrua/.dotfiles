92b893cddb80f1c96a43b34c76227949a860f79a
Stefan Rua <stefu.rua@gmail.com>
2018-12-07 22:36:06 +0200

    maybe improve this trash script? maybe break it. who knows

--- install.sh
+++ install.sh
@@ -1,13 +1,20 @@
 #! /bin/sh
 
+programs=(
+    bspwm
+    sxhkd
+    rofi
+    dunst
+    ranger
+    tint2
+)
+
 ln -s $PWD/.Xresources $HOME/
 
-for i in bspwm sxhkd rofi dunst ranger tint2
-do
-touch $HOME/.config/$i
+for i in $programs; do
+    touch $HOME/.config/$i
 done
 
-for i in bspwm sxhkd rofi dunst ranger tint2
-do
-ln -s $PWD/$i $HOME/.dotfiles/
+for i in $programs; do
+    ln -s $PWD/$i $HOME/.dotfiles/
 done
