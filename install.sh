85b36078acb4e44bd20c9130d9460966e39b5662
Stefan Rua <stefu.rua@gmail.com>
2019-01-26 21:22:56 +0000

    remove nvim and emacs ffs

--- install.sh
+++ install.sh
@@ -1,9 +1,8 @@
 #! /bin/bash
 
-programs="nvim bspwm sxhkd rofi dunst"
+programs="vim bspwm sxhkd rofi dunst"
 
 ln -s $PWD/.Xresources $HOME/
-ln -s $PWD/.emacs $HOME/
 
 for i in $programs; do
     ln -s $PWD/$i $HOME/.config/
