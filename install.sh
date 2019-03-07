d0091830cd20a85aaf60916e19d97c1e9fc673c6
Stefan Rua <stefu.rua@gmail.com>
2019-03-07 12:25:20 +0200

    Make script a little more verbose

--- install.sh
+++ install.sh
@@ -1,12 +1,18 @@
 #! /bin/bash
 
+dotfiles=".Xresources .vimrc .bashrc"
 programs="i3 i3status"
 
-ln -s $PWD/.Xresources $HOME/
-ln -s $PWD/.vimrc $HOME/
+for i in $dotfiles; do
+    echo "Symlinking $i..."
+    ln -s $PWD/$i $HOME/
+done
 
 for i in $programs; do
+    echo "Symlinking $i..."
     ln -s $PWD/$i $HOME/.config/
 done
+
+echo "Done!"
 
 xrdb $HOME/.Xresources
