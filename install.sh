8471edb19014acf45882c6c94b7f0c6d2d9d0e0c
Stefan Rua <stefu.rua@gmail.com>
2019-03-21 13:04:16 +0200

    Symlink only i3 related configs by default
    A new flag "-a" was added to the installer in case that all of my
    configs are wished to be installed.

--- install.sh
+++ install.sh
@@ -1,14 +1,20 @@
-#! /bin/bash
+#!/bin/bash
 
-dotfiles=".Xresources .vimrc .bashrc .tmux.conf"
-programs="i3 i3status"
+# Use the -a flag if you want all the configs I use
 
-for i in $dotfiles; do
+dotfiles=(".Xresources")
+programs=("i3" "i3status")
+
+if [ "$1" = "-a" ]; then
+    dotfiles+=(".vimrc" ".bashrc" ".tmux.conf")
+fi
+
+for i in ${dotfiles[@]}; do
     echo "Symlinking $i..."
     ln -s $PWD/$i $HOME/
 done
 
-for i in $programs; do
+for i in ${programs[@]}; do
     echo "Symlinking $i..."
     ln -s $PWD/$i $HOME/.config/
 done
